1bacccd24b95d2dd151386e979f7c1db6e7c1ca4
Ekaterina Chudinova <ek.chudi@ya.ru>
2025-04-14 19:50:14 +0300

    Работа с авторизацией в Playwright. Практика работы с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -2,7 +2,8 @@ from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
 
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
@@ -21,3 +22,29 @@ with sync_playwright() as playwright:
     dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
     expect(dashboard_title).to_be_visible()
     expect(dashboard_title).to_have_text('Dashboard')
+
+    context.storage_state(path="browser-state.json")
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+
+    courses_button = page.get_by_test_id('courses-drawer-list-item-button')
+    courses_button.click()
+
+    courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_visible()
+
+    courses_list_empty_icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(courses_list_empty_icon).to_be_visible()
+
+    courses_list_empty_title = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(courses_list_empty_title).to_be_visible()
+    expect(courses_list_empty_title).to_have_text('There is no results')
+
+    courses_list_empty_description = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(courses_list_empty_description).to_be_visible()
+    expect(courses_list_empty_description).to_have_text('Results from the load test pipeline will be displayed here')
