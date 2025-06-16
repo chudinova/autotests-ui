3d78f3328d8f4d8e2cc0c9f5a3acf25a2501bb4b
Ekaterina Chudinova <ek.chudi@ya.ru>
2025-06-16 20:09:33 +0300

    Практические применение PageFactory в автотестах. Урок

--- elements/input.py
+++ elements/input.py
@@ -4,17 +4,17 @@ from elements.base_element import BaseElement
 
 
 class Input(BaseElement):
-    def get_locator(self, **kwargs) -> Locator:
+    def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(**kwargs).locator('input')
 
-    def fill(self, value: str, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def fill(self, value: str, nth: int = 0, **kwargs):
+        locator = self.get_locator(nth, **kwargs)
         locator.fill(value)
 
-    def set_input_files(self, file: str, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def set_input_files(self, file: str, nth: int = 0, **kwargs):
+        locator = self.get_locator(nth, **kwargs)
         locator.set_input_files(file)
 
-    def check_have_value(self, value: str, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def check_have_value(self, value: str, nth: int = 0, **kwargs):
+        locator = self.get_locator(nth, **kwargs)
         expect(locator).to_have_value(value)
